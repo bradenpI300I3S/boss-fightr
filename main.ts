17863edc631fb5d65727812d5d4fdd2d348d7d9e
bradenpI300I3S <82404523+bradenpI300I3S@users.noreply.github.com>
2021-05-07 13:57:21 -0400

    Jail idea

--- main.ts
+++ main.ts
@@ -5,105 +5,106 @@ namespace StatusBarKind {
     export const SheildCooldown = StatusBarKind.create()
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (JumpCount < 2) {
-        mySprite.vy = -100
+    if (JumpCount < JumpStatus) {
+        mySprite.vy = -140
         JumpCount += 1
     }
 })
+function Character () {
+    mySprite = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(mySprite, 100, 0)
+    mySprite.ay = 195
+    scene.cameraFollowSprite(mySprite)
+    tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 27))
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    Shield = sprites.create(img`
-        . . f f f . . 
-        . f 8 8 8 f . 
-        . f 8 8 8 f . 
-        . f 8 9 8 f . 
-        f 8 8 8 9 8 f 
-        f 8 8 8 9 8 f 
-        f 8 8 8 8 8 f 
-        f 8 8 8 8 8 f 
-        f 8 8 8 8 8 f 
-        f 8 8 8 8 8 f 
-        . f 8 8 8 f . 
-        . f 8 8 8 f . 
-        . f 8 8 8 f . 
-        . . f f f . . 
-        `, SpriteKind.Protectoion)
-    Shield.setPosition(mySprite.x + 13, mySprite.y)
-    SheildCreated = true
+    if (true) {
+        Shield = sprites.create(img`
+            . . f f f . . 
+            . f 8 8 8 f . 
+            . f 8 8 8 f . 
+            . f 8 9 8 f . 
+            f 8 8 8 9 8 f 
+            f 8 8 8 9 8 f 
+            f 8 8 8 8 8 f 
+            f 8 8 8 8 8 f 
+            f 8 8 8 8 8 f 
+            f 8 8 8 8 8 f 
+            . f 8 8 8 f . 
+            . f 8 8 8 f . 
+            . f 8 8 8 f . 
+            . . f f f . . 
+            `, SpriteKind.Protectoion)
+        Shield.setPosition(mySprite.x + 13, mySprite.y)
+        SheildCreated = true
+    }
 })
 function Level1 () {
     tiles.setTilemap(tilemap`level1`)
 }
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    firstEffect()
+    mySprite.setImage(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 8 8 8 8 2 f d 4 . . 
+        . . 4 4 f 8 8 9 9 8 8 f 4 4 . . 
+        . . . . . 8 9 8 8 9 8 . . . . . 
+        . . . . . 8 8 . . 8 8 . . . . . 
+        `)
+})
 controller.A.onEvent(ControllerButtonEvent.Released, function () {
     Shield.destroy()
     SheildCreated = false
 })
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Protectoion, function (sprite, otherSprite) {
-    sprite.destroy(effects.disintegrate, 500)
-})
-let myEnemy: Sprite = null
+function firstEffect () {
+    JumpStatus = 2
+    pause(30000)
+    JumpStatus = 1
+}
 let SheildCreated = false
 let Shield: Sprite = null
-let JumpCount = 0
 let mySprite: Sprite = null
+let JumpStatus = 0
+let JumpCount = 0
 Level1()
-scene.cameraFollowSprite(mySprite)
-mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-mySprite.setPosition(20, 50)
-controller.moveSprite(mySprite, 100, 0)
+Character()
 JumpCount = 0
-mySprite.ay = 200
-forever(function () {
-    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
-        JumpCount = 0
-    }
-})
+JumpStatus = 1
 forever(function () {
     if (SheildCreated) {
         Shield.setPosition(mySprite.x + 13, mySprite.y)
     }
 })
-game.onUpdateInterval(500, function () {
-    myEnemy = sprites.create(img`
-        ........................
-        ........................
-        ........................
-        ..........ffff..........
-        ........ff1111ff........
-        .......fb111111bf.......
-        .......f1111111dbf......
-        ......fd1111111ddf......
-        ......fd111111dddf......
-        ......fd111ddddddf......
-        ......fd111ddddddf......
-        ......fd1dddddddbf......
-        ......fd1dfbddbbff......
-        ......fbddfcdbbcf.......
-        .....ffffccddbfff.......
-        ....fcb1bbbfcffff.......
-        ....f1b1dcffffffff......
-        ....fdfdf..ffffffffff...
-        .....f.f.....ffffff.....
-        ........................
-        ........................
-        ........................
-        ........................
-        ........................
-        `, SpriteKind.Player)
+forever(function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        JumpCount = 0
+    }
 })
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,20 +2,52 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1400080001010101010101010101010101010101010101010102010101020101010201010102010301020101010101030101010301010301010101010101010101010101010101010101010101010101010103010101010101010301010101030101010101010101010101030101010101010101010101030101010101020101010201010102010101020101010201010101010101010101010101030101010101010101`, img`
-. . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . . . . . 
-2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,sprites.dungeon.floorDark0,sprites.dungeon.floorDarkDiamond,sprites.dungeon.floorDark1], TileScale.Sixteen);
+            case "level1":return tiles.createTilemap(hex`14001e000101010101010101010101010101010101010101010101010101010101010101010101010101010101020101010201010102010101020101010201010101010101010101010101010101010101010101010101010101010101010101010101010101010101020101010201010102010101020101010201010101010101010101010101010101010101010101010101010101010101010101010101010101010101020101010201010102010101020101010201010101010101010101010101010101010101010101010101010101010101010101010101010101010101020101010201010102010101020101010201010101010101010101010101010101010101010101010101010101010101010101010101010101010101020101010201010102010101020101010213010101010101010101010101010111010101011201010101010101010101010101010b0101010f0f010102010101020d0101020101010b0101010201010101010101010b0101010101010b0101010101010101010101010b0101010101010b1001010101010102010101020c0101020101010b010101020101010101010101010101010101010b01010101010101010101010101090e0e0a01010b010f01010101010201010102010101020101010b0101010f0101010101010101010101010101010c01010101010101010101010101010101010101010101010101011402011401020101010201010102090e0e0a0101140101140101010101010101010101010101010108030303030303030303030303030303030303040706060606060606060606060606060606060605`, img`
+2..................2
+2..................2
+2..................2
+2..................2
+2..................2
+2..................2
+2..................2
+2..................2
+2..................2
+2..................2
+2..................2
+2..................2
+2..................2
+2..................2
+2..................2
+2............2.....2
+2............2...222
+2.....2......2.....2
+2.....2......2.....2
+2.....2......22....2
+2.....2......2.....2
+2............2.....2
+2......2222..2.2...2
+2............2...2.2
+2............2.....2
+2..................2
+2.............2222.2
+2..................2
+22222222222222222222
+22222222222222222222
+`, [myTiles.transparency16,sprites.dungeon.floorDark0,sprites.dungeon.floorDarkDiamond,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.doorOpenWest,sprites.dungeon.doorClosedEast,myTiles.tile2,sprites.dungeon.doorClosedSouth,sprites.dungeon.doorOpenNorth,myTiles.tile1,sprites.dungeon.floorLight0,sprites.dungeon.floorLight1,sprites.dungeon.doorClosedNorth,sprites.dungeon.chestClosed,myTiles.tile3,myTiles.tile4,myTiles.tile5], TileScale.Sixteen);
         }
         return null;
     })
@@ -23,6 +55,16 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
         }
         return null;
     })
